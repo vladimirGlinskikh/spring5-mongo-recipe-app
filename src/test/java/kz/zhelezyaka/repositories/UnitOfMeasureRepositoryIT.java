a45be7aca371da1846fe38815fa963b043455939
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-05-18 03:11:15 +0600

    Integration Testing with MongoDB

--- src/test/java/kz/zhelezyaka/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/kz/zhelezyaka/repositories/UnitOfMeasureRepositoryIT.java
@@ -1,11 +1,12 @@
 package kz.zhelezyaka.repositories;
 
+import kz.zhelezyaka.bootstrap.RecipeBootstrap;
 import kz.zhelezyaka.domain.UnitOfMeasure;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,16 +14,27 @@ import java.util.Optional;
 
 import static org.junit.Assert.*;
 
-@Ignore
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@DataMongoTest
 public class UnitOfMeasureRepositoryIT {
 
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
 
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    RecipeRepository recipeRepository;
+
     @Before
     public void setUp() throws Exception {
+        recipeRepository.deleteAll();
+        unitOfMeasureRepository.deleteAll();
+        categoryRepository.deleteAll();
+
+        RecipeBootstrap recipeBootstrap = new RecipeBootstrap(categoryRepository, recipeRepository, unitOfMeasureRepository);
+        recipeBootstrap.onApplicationEvent(null);
     }
 
     @Test
--- src/test/java/kz/zhelezyaka/services/IngredientServiceImplTest.java
+++ src/test/java/kz/zhelezyaka/services/IngredientServiceImplTest.java
@@ -77,7 +77,6 @@ public class IngredientServiceImplTest {
 
         //when
         assertEquals("3", ingredientCommand.getId());
-        assertEquals("1", ingredientCommand.getRecipeId());
         verify(recipeRepository, times(1)).findById(anyString());
     }
 
@@ -113,7 +112,6 @@ public class IngredientServiceImplTest {
         Ingredient ingredient = new Ingredient();
         ingredient.setId("3");
         recipe.addIngredient(ingredient);
-        ingredient.setRecipe(recipe);
         Optional<Recipe> recipeOptional = Optional.of(recipe);
 
         when(recipeRepository.findById(anyString())).thenReturn(recipeOptional);
