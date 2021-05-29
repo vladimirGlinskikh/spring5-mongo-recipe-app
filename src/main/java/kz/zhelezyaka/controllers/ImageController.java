efbbaacf97aa4d04e50c3134123d62ba7b5b6d7d
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-05-29 22:35:10 +0600

    Refactor image service

--- src/main/java/kz/zhelezyaka/controllers/ImageController.java
+++ src/main/java/kz/zhelezyaka/controllers/ImageController.java
@@ -25,30 +25,30 @@ public class ImageController {
 
     @GetMapping("recipe/{id}/image")
     public String showUploadForm(@PathVariable String id, Model model) {
-        model.addAttribute("recipe", recipeService.findCommandById(id));
+        model.addAttribute("recipe", recipeService.findCommandById(id).block());
         return "recipe/imageuploadform";
     }
 
     @PostMapping("recipe/{id}/image")
     public String handleImagePost(@PathVariable String id,
                                   @RequestParam("imagefile") MultipartFile file) {
-        imageService.saveImageFile(id, file);
+        imageService.saveImageFile(id, file).block();
         return "redirect:/recipe/" + id + "/show";
     }
 
-//    @GetMapping("recipe/{id}/recipeimage")
-//    public void renderImageFromDB(@PathVariable String id, HttpServletResponse response) throws Exception {
-//        RecipeCommand recipeCommand = recipeService.findCommandById(id);
-//
-//        if (recipeCommand.getImage() != null) {
-//            byte[] byteArray = new byte[recipeCommand.getImage().length];
-//            int i = 0;
-//            for (Byte wrappedByte : recipeCommand.getImage()) {
-//                byteArray[i++] = wrappedByte;
-//            }
-//            response.setContentType("image/jpeg");
-//            InputStream is = new ByteArrayInputStream(byteArray);
-//            IOUtils.copy(is, response.getOutputStream());
-//        }
-//    }
+    @GetMapping("recipe/{id}/recipeimage")
+    public void renderImageFromDB(@PathVariable String id, HttpServletResponse response) throws Exception {
+        RecipeCommand recipeCommand = recipeService.findCommandById(id).block();
+
+        if (recipeCommand.getImage() != null) {
+            byte[] byteArray = new byte[recipeCommand.getImage().length];
+            int i = 0;
+            for (Byte wrappedByte : recipeCommand.getImage()) {
+                byteArray[i++] = wrappedByte;
+            }
+            response.setContentType("image/jpeg");
+            InputStream is = new ByteArrayInputStream(byteArray);
+            IOUtils.copy(is, response.getOutputStream());
+        }
+    }
 }
--- src/main/java/kz/zhelezyaka/services/ImageServiceImpl.java
+++ src/main/java/kz/zhelezyaka/services/ImageServiceImpl.java
@@ -1,43 +1,42 @@
 package kz.zhelezyaka.services;
 
 import kz.zhelezyaka.domain.Recipe;
-import kz.zhelezyaka.repositories.RecipeRepository;
+import kz.zhelezyaka.repositories.reactive.RecipeReactiveRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
-    private final RecipeRepository recipeRepository;
+    private final RecipeReactiveRepository recipeReactiveRepository;
 
-    public ImageServiceImpl(RecipeRepository recipeRepository) {
-        this.recipeRepository = recipeRepository;
+    public ImageServiceImpl(RecipeReactiveRepository recipeReactiveRepository) {
+        this.recipeReactiveRepository = recipeReactiveRepository;
     }
 
     @Override
-    @Transactional
-    public void saveImageFile(String recipeId, MultipartFile file) {
-        log.debug("Received a file");
-        try {
-            Recipe recipe = recipeRepository.findById(recipeId).get();
-            Byte[] byteObjects = new Byte[file.getBytes().length];
-            int i = 0;
-
-            for (byte b : file.getBytes()) {
-                byteObjects[i++] = b;
-            }
-
-            recipe.setImage(byteObjects);
-
-            recipeRepository.save(recipe);
-        } catch (IOException e) {
-            //todo handle better
-            log.error("Error occurred", e);
-            e.printStackTrace();
-        }
+    public Mono<Void> saveImageFile(String recipeId, MultipartFile file) {
+       Mono<Recipe> recipeMono = recipeReactiveRepository.findById(recipeId)
+                .map(recipe -> {
+                    Byte[] byteObjects = new Byte[0];
+                    try {
+                        byteObjects = new Byte[file.getBytes().length];
+                        int i = 0;
+                        for (byte b : file.getBytes()) {
+                            byteObjects[i++] = b;
+                        }
+                        recipe.setImage(byteObjects);
+                        return recipe;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        throw new RuntimeException(e);
+                    }
+                });
+        recipeReactiveRepository.save(recipeMono.block()).block();
+        return Mono.empty();
     }
 }
