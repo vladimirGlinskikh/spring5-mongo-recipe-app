76ce5242d787eb0f13b4fe4fe3552174ff289e2c
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-09-04 23:03:41 +0600

    WebFlux Exception Handling

--- src/main/java/kz/zhelezyaka/controllers/RecipeController.java
+++ src/main/java/kz/zhelezyaka/controllers/RecipeController.java
@@ -1,13 +1,16 @@
 package kz.zhelezyaka.controllers;
 
 import kz.zhelezyaka.commands.RecipeCommand;
+import kz.zhelezyaka.exceptions.NotFoundException;
 import kz.zhelezyaka.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.exceptions.TemplateInputException;
 
 @Slf4j
 @Controller
@@ -66,15 +69,13 @@ public class RecipeController {
         return "redirect:/";
     }
 
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    @ExceptionHandler(NotFoundException.class)
-//    public ModelAndView handleNotFound(Exception exception) {
-//        log.error("Handling not found exception");
-//        log.error(exception.getMessage());
-//
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("404error");
-//        modelAndView.addObject("exception", exception);
-//        return modelAndView;
-//    }
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({NotFoundException.class, TemplateInputException.class})
+    public String handleNotFound(Exception exception, Model model) {
+        log.error("Handling not found exception");
+        log.error(exception.getMessage());
+
+        model.addAttribute("exception", exception);
+        return "404error";
+    }
 }
