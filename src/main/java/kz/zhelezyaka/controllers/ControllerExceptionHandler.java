4804ba4505a93d88991952b6fd3c5ccfed041fee
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-09-04 23:47:04 +0600

    WebFlux Controller Advice

--- src/main/java/kz/zhelezyaka/controllers/ControllerExceptionHandler.java
+++ src/main/java/kz/zhelezyaka/controllers/ControllerExceptionHandler.java
@@ -2,23 +2,22 @@ package kz.zhelezyaka.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-//import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ExceptionHandler(NumberFormatException.class)
-//    public ModelAndView handleNumberFormat(Exception exception) {
-//        log.error("Handling number format exception");
-//        log.error(exception.getMessage());
-//
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("400error");
-//        modelAndView.addObject("exception", exception);
-//        return modelAndView;
-//    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WebExchangeBindException.class)
+    public String handleNumberFormat(Exception exception, Model model) {
+        log.error("Handling binding exception");
+        log.error(exception.getMessage());
+
+        model.addAttribute("exception", exception);
+        return "400error";
+    }
 }
